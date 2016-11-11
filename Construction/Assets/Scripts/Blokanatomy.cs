f430b96bcd94b12dcb84d1af78689a3210c1db02
autonomnom <christopf@posteo.de>
2016-11-11 18:21:40 +0100

    getting blok data to the bloks, making prefabs, gather blok data in another space

--- Construction/Assets/Scripts/Blokanatomy.cs
+++ Construction/Assets/Scripts/Blokanatomy.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Blokanatomy : MonoBehaviour {
 
-	// Use this for initialization
+	// dimensions of existence
+	public bool[,,] koerper;
+
+	// name for finding in enzyklopedia
+	//public string namae = this.name;
+
+	// 0 = player1, 1 = player2, 2 = player3, 3 = player4
+	public int zugehoergig;
+
 	void Start () {
-	
+
+		koerper = Enzyklopedia.instance.peter [name];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		Debug.Log (koerper);
 	}
 }
--- Construction/Assets/Scripts/Gridcircus.cs
+++ Construction/Assets/Scripts/Gridcircus.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Gridcircus : MonoBehaviour {
 
-	// Use this for initialization
+	public int gridMaxUnits = 9;
+	public int gridSize = 50;
+
 	void Start () {
 	
+
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+
+		//Drawing the invisible grid 
+		for (int i = 0; i <= 9; i++) {
+
+			Debug.DrawLine (new Vector3 (i*gridSize, 0, 0), new Vector3 (i*gridSize, 0, gridMaxUnits*gridSize), Color.red);
+			Debug.DrawLine (new Vector3 (0, 0, i*gridSize), new Vector3 (gridMaxUnits*gridSize, 0, i*gridSize), Color.red);
+		}
+
+
 	}
 }
