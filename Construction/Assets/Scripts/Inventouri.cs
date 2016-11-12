a28a7034b0abd4b864bf22d452c4e00be58fd0c6
stilllovinnero <krisekrise@posteo.de>
2016-11-12 01:37:27 +0100

    Revert "inventories"
    This reverts commit 7f08b3fd6758f899de925683a5b59e41775ad338.

--- Construction/Assets/Scripts/Inventouri.cs
+++ Construction/Assets/Scripts/Inventouri.cs
@@ -3,42 +3,13 @@ using System.Collections;
 
 public class Inventouri : MonoBehaviour {
 
-	GameObject[] inventoryItems = new GameObject[3];
-
-	GameObject[] playerItems = new GameObject[4];
-
-	// fill teh pool
-	GameObject[] itemPool;
-
-
 	// Use this for initialization
 	void Start () {
-
-		itemPool = Resources.LoadAll ("Bloks");
-
-		// fill the Arrays :3
-		for (int i = 0; i < inventoryItems.GetLength (); i++) {
-			inventoryItems [i] = GetRandItem ();
-		}
-		for (int i = 0; i < playerItems.GetLength (); i++) {
-			playerItems [i] = GetRandItem ();
-		}
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	GameObject GetRandItem(){
-		return itemPool[Random.Range(0,itemPool.GetLength())];
-	}
-
-	GameObject swapItems(int playerID, int inventoryID){
-		return inventoryItems [inventoryID];
-		GameObject placeholder = inventoryItems [inventoryID];
-		inventoryItems [inventoryID] = playerItems [playerID];
-		playerItems [playerID] = placeholder;
 	}
 }
