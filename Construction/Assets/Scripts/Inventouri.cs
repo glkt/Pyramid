c3c0726263f1eb55aef26b035d05df686fce3cd8
stilllovinnero <krisekrise@posteo.de>
2016-11-12 01:41:37 +0100

    inevtiuor

--- Construction/Assets/Scripts/Inventouri.cs
+++ Construction/Assets/Scripts/Inventouri.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class Inventouri : MonoBehaviour {
 
+	public GameObject[] inventoryItems = new GameObject[3];
+
+	public GameObject[] playerItems = new GameObject[4];
+
+	// fill teh pool
+	GameObject[] itemPool;
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+
+		itemPool = Resources.LoadAll<GameObject>("Bloks");
+
+		// fill the Arrays :3
+		for (int i = 0; i < inventoryItems.GetLength(0); i++) {
+			inventoryItems [i] = GetRandItem ();
+		}
+		for (int i = 0; i < playerItems.GetLength (0); i++) {
+			playerItems [i] = GetRandItem ();
+		}
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	GameObject GetRandItem(){
+		return itemPool[Random.Range(0,itemPool.GetLength(0))];
+	}
+
+	public GameObject swapItems(int playerID, int inventoryID){
+		return inventoryItems [inventoryID];
+		GameObject placeholder = inventoryItems [inventoryID];
+		inventoryItems [inventoryID] = playerItems [playerID];
+		playerItems [playerID] = placeholder;
+	}
+
+	public void refill(int playerID, int inventoryID){
+		playerItems [playerID] = inventoryItems [inventoryID];
+		inventoryItems [inventoryID] = GetRandItem();
 	}
 }
